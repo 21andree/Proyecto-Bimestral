efd6989cb93c98f4bc24d984c1e97ce6e1b666d3
Pablo Armijos <pablo.armijos1109@gmail.com>
2026-05-24 18:48:49 -0500

    Controlador-Repositorio

--- Solucion_Codigo/src/controlador/Repositorio.java
+++ Solucion_Codigo/src/controlador/Repositorio.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
-/**
- *
- * @author ASUS
- */
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import modelo.PatioComidas;
+
 public class Repositorio {
-    
+
+    private String nombreArchivo = "patio.dat";
+
+    public void guardar(PatioComidas patio) {
+        try (ObjectOutputStream write = new ObjectOutputStream(
+                new FileOutputStream(nombreArchivo))) {
+            write.writeObject(patio);
+        } catch (IOException e) {
+            System.out.println("Error al guardar los datos: " + e.getMessage());
+        }
+    }
+
+    public PatioComidas cargar() {
+        try (ObjectInputStream read = new ObjectInputStream(
+                new FileInputStream(nombreArchivo))) {
+            return (PatioComidas) read.readObject();
+        } catch (FileNotFoundException e) {
+            return null;
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("Error al cargar los datos: " + e.getMessage());
+            return null;
+        }
+    }
 }
